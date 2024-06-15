fcb63957f40b5b077c0432c161c1cd156923cabc
Ca7Ac1 <Ca7Ac1@gmail.com>
2024-06-15 15:20:07 -0400

    stuff

---
+++ stuff.py
@@ -0,0 +1,11 @@
+ import numpy as np
+ import cv2
+ import pyclear
+ vi stuff.py
+ iimport numpy as np
+ import cv2
+ import pyautogui
+
+ image = pyautogui.screenshot()
+
+ image1 = pyautogui.screenshot("image1.png")
